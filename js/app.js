98f418329d01d047ca8c3dafce6c81b0e5d01228
phungductrong <phungductrong1999@gmail.com>
2024-04-15 21:39:25 +0700

    fix style for project

--- js/app.js
+++ js/app.js
@@ -1,44 +1,34 @@
-function openNavbar() {
-    document.getElementById("sideNavigationBar")
-        .style.width = "50%";
-}
-function closeNavbar() {
-    document.getElementById("sideNavigationBar")
-        .style.width = "0%";
-}
-
 // Define a function to handle scrolling to the corresponding section when clicking the button in the navigation
 function scrollToSection() {
-    // Get all the buttons in the navigation
-    const buttons = document.querySelectorAll('.navbar a');
+  // Get all the buttons in the navigation
+  const buttons = document.querySelectorAll(".navbar a");
 
-    // Loop through each button and add a click event to each button
-    buttons.forEach(function(button) {
-        button.addEventListener('click', function(event) {
-            
-            event.preventDefault();
+  // Loop through each button and add a click event to each button
+  buttons.forEach(function (button) {
+    button.addEventListener("click", function (event) {
+      event.preventDefault();
 
-            // Delete class 'active' of all button
-            buttons.forEach(function(btn) {
-                btn.classList.remove('active');
-            });
+      // Delete class 'active' of all buttons
+      buttons.forEach(function (btn) {
+        btn.classList.remove("active");
+      });
 
-            // Add class 'active' in current button
-            button.classList.add('active');
+      // Add class 'active' in current button
+      button.classList.add("active");
 
-            //Get the id of the section corresponding to the clicked button
-            const sectionId = button.textContent.toLowerCase().replace(/\s+/g, '');
+      //Get the id of the section corresponding to the clicked button
+      const sectionId = button.textContent.toLowerCase().replace(/\s+/g, "");
 
-            // Find element section
-            const section = document.getElementById(sectionId);
+      // Find element section
+      const section = document.getElementById(sectionId);
 
-            // scroll to the section's location
-            section.scrollIntoView({
-                behavior: 'smooth'
-            });
-        });
+      // scroll to the section's location
+      section.scrollIntoView({
+        behavior: "smooth",
+      });
     });
+  });
 }
 
 // call function scrollToSection to execute processing when the document is loaded
-document.addEventListener('DOMContentLoaded', scrollToSection);
+document.addEventListener("DOMContentLoaded", scrollToSection);
